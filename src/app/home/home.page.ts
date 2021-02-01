61f7437e89bd041a66982310c2020819ba9c8a3b
Makwale <makwale.em@gmail.com>
2021-02-01 04:30:46 -0800

    Fist commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private localNotifications: LocalNotifications) {}
+
+  test(){
+    this.localNotifications.schedule({
+      id: 1,
+      text: 'Single ILocalNotification',
+      sound: 'file://sound.mp3',
+      title: 'My Fist Notification'
+    });
+  }
 
 }
